16f742236a70964369a05785ec2d01281957cda6
gile <dragisamirkovic@gmail.com>
2019-02-18 18:13:15 +0100

    readme & build

---
+++ build/precache-manifest.030399e70ceda251a29998568d314590.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f7c9454ad547092f645f",
+    "url": "/static/js/main.f7c9454a.chunk.js"
+  },
+  {
+    "revision": "c7d8916fa7d650ab1ce4",
+    "url": "/static/js/2.c7d8916f.chunk.js"
+  },
+  {
+    "revision": "f7c9454ad547092f645f",
+    "url": "/static/css/main.1169eb0d.chunk.css"
+  },
+  {
+    "revision": "e250ca688f884e4132da7231876cf877",
+    "url": "/index.html"
+  }
+];
